e38352c0e0fa786aab4e389a4a0a28d32529ee13
Rik <rik.pie@gmail.com>
2018-12-18 21:27:54 +0500

    first commit

---
+++ Testing Transport Task/Testing Transport Task/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Testing_Transport_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
